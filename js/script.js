0d2949829a315bd1c7472d837912a6a90a01e33a
jperez <jperez4509@gmail.com>
2015-07-20 13:00:24 -0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,28 +1,34 @@
-//var stringArray = [];
-//
-//function userPrompt(){
-//    var userInput = prompt('Please enter a sentece.');
-//    stringArray.push(userInput);
-//}
-//
-//userPrompt();
-//console.log(stringArray);
+var stringArray = [];
 
-var h1 = document.getElementById("myHdr");
-console.log(h1);
-
-var hasClass = h1.hasAttribute("class");
-console.log("The h1 tag has a class attribute: ", hasClass);
-
-h1.setAttribute("rel", "something");
-var hasRel = h1.hasAttribute("rel");
-console.log("The h1 tag has a rel attribute: ", hasRel);
-
-var anchors = document.querySelectorAll("ul li a");
-console.log(anchors);
-console.log(anchors.length);
-
-for (var i = 0; i < anchors.length; i++) {
-    var anchor = anchors.item(i);
-    console.log(anchor.href);
+function userPrompt(){
+    var userInput = prompt('Please enter a sentece.');
+    stringArray.push(userInput);
 }
+
+userPrompt();
+
+var left = document.querySelector("#left");
+var p = document.createElement('p');
+
+left.appendChild(p).textContent = stringArray;
+
+
+//
+//var h1 = document.getElementById("myHdr");
+//console.log(h1);
+//
+//var hasClass = h1.hasAttribute("class");
+//console.log("The h1 tag has a class attribute: ", hasClass);
+//
+//h1.setAttribute("rel", "something");
+//var hasRel = h1.hasAttribute("rel");
+//console.log("The h1 tag has a rel attribute: ", hasRel);
+//
+//var anchors = document.querySelectorAll("ul li a");
+//console.log(anchors);
+//console.log(anchors.length);
+//
+//for (var i = 0; i < anchors.length; i++) {
+//    var anchor = anchors.item(i);
+//    console.log(anchor.href);
+//}
